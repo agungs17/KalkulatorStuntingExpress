a4037e54dfcf559aa620302dacaae9a55b417f33
agungs <agungs1709@gmail.com>
2025-05-19 02:27:48 +0700

    fix : auth controller

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,3 +1,5 @@
+import { formatResponse } from "../utils/scripts";
+
 export const loginController = (req, res) => {
   return formatResponse({ identifier: 'login', res, msgSuccess: "Login successful!" });
 }
