0c8c0515427f9eb01a0c54680441779a7c6df572
agungs <agungs1709@gmail.com>
2025-05-18 23:48:53 +0700

    feat : add vercel.json

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,13 +1,12 @@
-// esbuild.config.js
 const { build } = require('esbuild');
 
 build({
   entryPoints: ['src/index.js'],
   bundle: true,
   platform: 'node',
-  target: ['node18'], // Railway umumnya pakai node 18
+  target: ['node18'],
   outdir: 'dist',
-  format: 'cjs', // CommonJS agar tidak error dynamic require
+  format: 'cjs',
   sourcemap: true,
   logLevel: 'info',
   external: ['express', 'dotenv']
