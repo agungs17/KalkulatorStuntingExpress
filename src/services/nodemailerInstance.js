ff023852656ab675e90be0924bfa7147902db92b
agungs <agungs1709@gmail.com>
2025-05-27 17:16:44 +0700

    fix nodemailerInstance

--- src/services/nodemailerInstance.js
+++ src/services/nodemailerInstance.js
@@ -3,11 +3,12 @@ import { supabaseAxiosInstance } from "./axiosInstance";
 const sendEmail = ({ to, subject, html }) => {
   if(!to || !subject || !html) throw new Error("to, subject, and html are required parameters");
   
-  supabaseAxiosInstance.post("/functions/v1/send-email", { to, subject, html }).then(response => {
-    if (response.status !== 200) throw new Error(`Failed to send email: ${response.error}`);
-    return response.data;
-  }).catch(error => {
-    throw error;
+  supabaseAxiosInstance.post("/functions/v1/send-email", { to, subject, html })
+    .then(response => {
+      console.log("sendEmail: Success", response?.data?.message);
+    })
+    .catch(error => {
+      console.error("sendEmail: Error sending email", String(error));
   });
 };
 
