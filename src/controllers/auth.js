c20ff9dcbf2b6d80deba4c180923b41a19875c1f
agungs <agungs1709@gmail.com>
2025-05-28 02:25:16 +0700

    fix : register type

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -47,7 +47,7 @@ export const registerController = async (req, res) => {
     }
 
     if (code === 200 && id && useNodemailer) {
-      const type = EMAIL_TYPE["forgot-password-email"].value;
+      const type = EMAIL_TYPE["verification-email"].value;
       const {token, expiredLabel, expiredDatetime} = generateToken({ id, type });
       await supabaseInstance
         .from('tokens_table')
