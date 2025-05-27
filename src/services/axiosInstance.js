62f5b843aa0831ab3f4c8dbe7fd4691201341b8b
agungs <agungs1709@gmail.com>
2025-05-28 02:12:39 +0700

    remove : remove lib axios

--- src/services/axiosInstance.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-import config from "../configurations";
-
-const axiosCreate = (baseURL, headers = {}, timeout = 5000) => axios.create({ baseURL, headers, timeout });
-
-const supabaseAxiosInstance = axiosCreate(config.supabase?.url, { Authorization: `Bearer ${config.supabase?.serviceRoleKey}`, "Content-Type": "application/json" }, 5000);
-
-export { supabaseAxiosInstance };
