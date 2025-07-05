f791b053a28b8dcda500400ef14ec0e74be1fb27
agungs <agungs1709@gmail.com>
2025-07-06 04:28:27 +0700

    rm middleware

--- api/_middleware.js
+++
@@ -1,9 +0,0 @@
-export default function middleware() {
-  return new Response(
-    JSON.stringify({ message: "Blocked by middleware" }),
-    {
-      status: 403,
-      headers: { "content-type": "application/json" },
-    }
-  );
-}
