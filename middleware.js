d99a81a04eb3072798d1fc7a70e110d17208edf7
agungs <agungs1709@gmail.com>
2025-07-12 06:44:26 +0700

    test middleware

---
+++ middleware.js
@@ -0,0 +1,26 @@
+export const config = {
+  matcher: "/api/:path*" // 🟢 intercept semua /api/*
+};
+
+export default function middleware(req) {
+  console.log("🔥 Middleware triggered:", req.url);
+
+  const maintenance = false;
+
+  if (maintenance) {
+    return new Response(
+      JSON.stringify({
+        code: 503,
+        message: "Server sedang maintenance, coba lagi nanti.",
+        data: null,
+        error: "ServiceUnavailable"
+      }),
+      {
+        status: 503,
+        headers: { "Content-Type": "application/json" }
+      }
+    );
+  }
+
+  return Response.next();
+}
