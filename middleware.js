04b21cd3a845b93b85b5a71914efbecd02f859ad
agungs <agungs1709@gmail.com>
2025-07-12 21:21:22 +0700

    simplify middleware

--- middleware.js
+++ middleware.js
@@ -1,20 +1,24 @@
-import { next, rewrite } from "@vercel/functions";
+import { next } from "@vercel/functions";
 
 export const config = {
   matcher: ["/:path*"]
 };
 
-export default function middleware(req) {
+export default async function middleware(req) {
   const { pathname } = new URL(req.url);
 
   if (pathname.startsWith("/api")) {
-    // untuk api (ada tambahan arcjet nanti)
+    // nanti akan ada tambahan untuk arcjet
     return next();
   }
 
   if (pathname === "/") {
-    return rewrite(new URL("/landing-page.html", req.url));
+    return new Response("<h1>Hello!</h1>", {
+      status: 200,
+      headers: {
+        "Content-Type": "text/html",
+      },
+    });
   }
-
   return next();
 }
