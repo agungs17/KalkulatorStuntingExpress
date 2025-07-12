110617a6749744ea22ac578c42bb7789dc5b4cce
agungs <agungs1709@gmail.com>
2025-07-12 20:52:03 +0700

    test middleware

--- middleware.js
+++ middleware.js
@@ -1,44 +1,19 @@
-import { next } from "@vercel/functions";
-// import { Arcjet } from "@arcjet/vercel";
-import landingHtml from "./public/html/landing-page.html";
-
-// const arcjet = new Arcjet({
-//   token: process.env.ARCJET_TOKEN
-// });
+import { next, rewrite } from "@vercel/functions";
 
 export const config = {
   matcher: ["/:path*"]
 };
 
-export default async function middleware(req) {
+export default function middleware(req) {
   const { pathname } = new URL(req.url);
 
-  // ✅ Kalau request ke /api → teruskan ke backend
   if (pathname.startsWith("/api")) {
-    // Kalau mau aktifkan Arcjet, uncomment kode di bawah
-    // const result = await arcjet.protect(req);
-    // if (!result.ok) {
-    //   return new Response(
-    //     JSON.stringify({
-    //       success: false,
-    //       message: "Request blocked by Arcjet",
-    //     }),
-    //     {
-    //       status: result.status,
-    //       headers: { "Content-Type": "application/json" },
-    //     }
-    //   );
-    // }
+    // untuk api (ada tambahan arcjet nanti)
     return next();
   }
 
   if (pathname === "/") {
-    return new Response(landingHtml, {
-      status: 200,
-      headers: {
-        "Content-Type": "text/html",
-      },
-    });
+    return rewrite(new URL("/landing-page.html", req.url));
   }
 
   return next();
