e5fc7ea83e7b39fc886aeda1643544f266f5202e
agungs <agungs1709@gmail.com>
2025-07-12 21:26:10 +0700

    test middleware

--- middleware.js
+++ middleware.js
@@ -8,11 +8,13 @@ export default async function middleware(req) {
   const { pathname } = new URL(req.url);
 
   if (pathname.startsWith("/api")) {
+    console.log("API request detected:", pathname);
     // nanti akan ada tambahan untuk arcjet
     return next();
   }
 
   if (pathname === "/") {
+    console.log("Root path accessed, returning custom response.");
     return new Response("<h1>Hello!</h1>", {
       status: 200,
       headers: {
@@ -20,5 +22,6 @@ export default async function middleware(req) {
       },
     });
   }
+  console.log("Non-API request detected, proceeding to next middleware.");
   return next();
 }
