492002e25f4e7339e956fee1df6db639106c865b
agungs <agungs1709@gmail.com>
2025-07-12 21:38:10 +0700

    test middleware

--- middleware.js
+++ middleware.js
@@ -1,24 +1,27 @@
-import { rewrite } from "@vercel/functions";
+import { next } from "@vercel/functions";
 
 export const config = {
   matcher: ["/:path*"]
 };
 
-export default function middleware(req) {
+export default async function middleware(req) {
   const { pathname } = new URL(req.url);
 
   if (pathname.startsWith("/api")) {
-    console.log("🔥 Rewriting API request:", pathname);
-    return rewrite(new URL(pathname, req.url));
+    // nanti akan ada tambahan untuk arcjet
+    console.log("API request detected:", pathname);
+    return;
   }
 
   if (pathname === "/") {
+    console.log("Root path accessed, returning custom response.");
     return new Response("<h1>Hello!</h1>", {
       status: 200,
-      headers: { "Content-Type": "text/html" },
+      headers: {
+        "Content-Type": "text/html",
+      },
     });
   }
-
-  console.log("Non-API request passthrough:", pathname);
-  return rewrite(new URL(pathname, req.url));
+  console.log("Non-API request detected, proceeding to next middleware.");
+  return next();
 }
