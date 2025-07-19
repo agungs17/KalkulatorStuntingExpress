969efd26df3cc28700211dd47a57242084ecd6f9
agungs <agungs1709@gmail.com>
2025-07-20 04:12:23 +0700

    feat : add qstash worker

--- src/services/workerInstance.js
+++ src/services/workerInstance.js
@@ -6,9 +6,16 @@ const qstash = new Client({
   token: config.upstashQStash.token
 });
 
-const workerInstance = async({req, path, body}) => {
+const workerInstance = async ({ req, path, body }) => {
   const baseUrl = getBaseUrl(req);
-  return await qstash.publish({ url: `${baseUrl}${path}`, body});
+  return await qstash.publish({
+    url: `${baseUrl}${path}`,
+    body,
+    headers: {
+      "Content-Type": "application/json"
+    },
+  });
 };
+
 
 export default workerInstance;
