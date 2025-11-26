c6526d0f23486d434ef15ac0895bbf7c9ceae2d0
agungs <agung.setiadi@ruparupa.com>
2025-11-26 21:57:08 +0700

    fix : logflare

--- src/services/logflareInstance.js
+++ src/services/logflareInstance.js
@@ -9,12 +9,12 @@ const logflareInstance = async (req, result) => {
   const fullPath = getBaseUrl(req) + path;
 
   const header = req?.headers || {};
-  const body = sanitizeObject(req?.body, ["password", "old_password", "confirmation_password"], "***") || {};
+  const body = sanitizeObject(req?.body, ["password", "old_password", "confirmation_password"], "***") ?? {};
 
   try {
     const payload = {
       event_message: fullPath || "No path provided",
-      metadata: { header, body, result } ?? {},
+      metadata: { header, body, result } || {},
     };
 
     const res = await fetch(`https://api.logflare.app/logs?source=${config.logflare.sourceToken}`, {
